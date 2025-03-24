166a7c1fc73e6d05bbde77455c0e8c409fa1205c
Samyak <samyak15.sj@gmail.com>
2025-03-24 13:17:36 +0530

    added splash screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,10 +13,32 @@ export const metadata: Metadata = {
   description: 'Split expenses with friends and family beautifully',
   manifest: '/manifest.json',
   icons: {
+    icon: [
+      { url: '/icons/icon-192x192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/icons/icon-512x512.png', sizes: '512x512', type: 'image/png' },
+    ],
     apple: [
       { url: '/icons/icon-192x192.png', sizes: '192x192', type: 'image/png' },
       { url: '/icons/icon-512x512.png', sizes: '512x512', type: 'image/png' },
     ],
+    shortcut: [
+      { url: '/icons/icon-192x192.png', sizes: '192x192', type: 'image/png' },
+    ],
+  },
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'black-translucent',
+    title: 'SplitSmart',
+  },
+  applicationName: 'SplitSmart',
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: 'website',
+    siteName: 'SplitSmart',
+    title: 'SplitSmart - Smart Expense Splitting',
+    description: 'Split expenses with friends and family beautifully',
   },
 };
 
@@ -46,7 +68,6 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-title" content="SplitSmart" />
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
-        {/* <meta name="msapplication-config" content="/browserconfig.xml" /> */}
         <meta name="msapplication-TileColor" content="#000000" />
         <meta name="msapplication-tap-highlight" content="no" />
         <meta name="theme-color" content="#000000" />
@@ -54,8 +75,12 @@ export default function RootLayout({
         <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000000" />
         <meta name="apple-touch-fullscreen" content="yes" />
         <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="apple-touch-startup-image" content="/icons/splash.png" />
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/icons/icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/icon-192x192.png" />
+        <link rel="apple-touch-icon" sizes="167x167" href="/icons/icon-152x152.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/icon-48x48.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/icon-48x48.png" />
         <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#000000" />
         <link rel="manifest" href="/manifest.json" crossOrigin="use-credentials" />
       </head>
